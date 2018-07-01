378c5e377f79074536f4d9d7cdb5a01f73e9047c
Hamilton Chevez <hchev001@fiu.edu>
2018-07-01 16:34:56 -0400

    initial commit of SimpleList1

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/appStyles.scss";
+
+const Index = () => {
+  return (
+    <div className="demo">
+      <h1 className="example">Demo</h1>!
+    </div>
+  );
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
